e91cd364e03e84179d2c326b40e788c6a36a0e9a
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-03-15 22:32:14 -0400

    changes

---
+++ Auction/SimpleAuction.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+
+contract SimpleAuction{
+    address payable public beneficiary;
+    uint public auctionEndTime;
+
+
+    address public highestBidder;
+    uint public highestBid;
+
+    mapping(address => uint) pendingReturns;
+
+
+    bool ended;
+
+
+    event HighestBidIncreased(address bidder, uint amount);
+    event AuctionEnded(address winner, uint amount);
+
+    error AuctionAlreadyEnded();
+    error BidNotHighEnough(uint highestBid);
+    error AuctionNotYetEnded();
+    error AuctionEndAlreadyCalled();
+
+
+    constructor(uint biddingTime, address payable beneficiaryAddress){
+        beneficiary = beneficiaryAddress;
+        auctionEndTime = block.timestamp + biddingTime;
+    }
+
+    function bid() external payable {
+        if(block.timestamp > auctionEndTime)
+            revert AuctionAlreadyEnded();
+
+        if(msg.value <= highestBid)
+            revert BidNotHighEnough(highestBid);
+
+        if(highestBid != 0){
+            pendingReturns[highestBidder] += highestBid;
+        }
+
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+        emit HighestBidIncreased(highestBidder, highestBid);
+    }
+
+    function withdraw() external returns (bool){
+        uint amount = pendingReturns[msg.sender];
+        if(amount > 0){
+            pendingReturns[msg.sender] = 0;
+
+            if(!payable(msg.sender).send(amount)){
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+            return true;
+        }
+    }
+
+    function auctionEnd() external{
+        if(block.timestamp < auctionEndTime)
+            revert AuctionNotYetEnded();
+        if(ended)
+            revert AuctionAlreadyEnded();
+        
+        ended = true;
+        emit AuctionEnded(highestBidder, highestBid);
+
+        beneficiary.transfer(highestBid);
+    }
+
+
+}
